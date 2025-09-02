0c6cf74631f0200fee90a0d88b60a7d2e1d280e8
Leonardo Arcanjo <leonardoarcanjoo@gmail.com>
2025-09-02 14:53:03 -0400

    feat: using stream operation to get statistics and use findFirst

--- src/main/java/br/com/alura/screematch/main/UserInterface.java
+++ src/main/java/br/com/alura/screematch/main/UserInterface.java
@@ -31,7 +31,7 @@ public class UserInterface {
         System.out.println("Serie " + tvSerieName + ":");
         System.out.println(serie);
 
-        List<Episode> allEpisodes = new ArrayList<>();
+//        List<Episode> allEpisodes = new ArrayList<>();
         List<EpisodeObject> episodes = new ArrayList<>();
 
         // Show Season and episodes by seasons data
@@ -53,17 +53,20 @@ public class UserInterface {
                     .collect(Collectors.toList()));
         }
 
-        System.out.println("Best episodes of " + tvSerieName);
+        // print all Episodes (in EpisodesObject way)
+        // episodes.forEach(System.out::println);
+
         // Show top 5 episodes by rating
-        allEpisodes.stream()
-                .filter(ep -> !ep.rating().equals("N/A"))           // remove N/A rating
-                .sorted(Comparator.comparing(Episode::rating).reversed())   // decrescent sorted
-                .limit(5)
-                .forEach(System.out::println);
+        /*allEpisodes.stream()
+                .filter(ep -> !ep.rating().equals("N/A"))                   // remove N/A rating
+                .peek(e -> System.out.println("after filter: " + e))        // show result of last stream operation
+                .sorted(Comparator.comparing(Episode::rating).reversed())           // decrescent sorted
+                .limit(10)
+                .map(e -> e.title().toUpperCase())
+                .forEach(System.out::println);*/
 
-        episodes.forEach(System.out::println);
-
-        System.out.println("A partir de que ano voce deseja ver os episodios mais bem avaliados? ");
+        // Show episodes list after a year typed by user
+        /*System.out.println("A partir de que ano voce deseja ver os episodios mais bem avaliados? ");
         var year = reader.nextInt();
         reader.nextLine();
 
@@ -78,6 +81,60 @@ public class UserInterface {
                         "Season: " + e.getSeason() +
                                 " Episode: " + e.getTitle() +
                                 " Release Date: " + e.getReleaseDate().format(dateFormater)
-                ));
+                ));*/
+
+        // Found an episode typed by user
+        /*System.out.println("Type the episode are you find?");
+        var searchedEpisode = reader.nextLine();
+
+        // String handler
+        StringBuilder myEp = new StringBuilder();
+        String[] words = searchedEpisode.split(" ");
+
+        for (String word : words){
+            if (!word.isEmpty()){
+                myEp.append(word.substring(0, 1).toUpperCase())
+                        .append(word.substring(1).toLowerCase())
+                        .append(" ");
+            }
+        }
+
+        var ep = myEp.toString().trim();        // clear the last ` ` append in the last word
+
+        Optional<EpisodeObject> myFavoriteEpisode = episodes.stream()
+                .filter(e -> e.getTitle().contains(ep))
+                .findFirst();    // Return the first element in a search predicate, it`s a final stream operation and return an Optional<T>
+
+        if (myFavoriteEpisode.isPresent())
+            System.out.println("My search episode found: " + myFavoriteEpisode);
+        else
+            System.out.println("Episode not found!");*/
+
+        // Get Rating per Season
+        /*Map<Integer, Double> ratingPerSeason = episodes.stream()
+                .filter(e -> e.getRating() > 0.0)
+                .collect(Collectors.groupingBy(EpisodeObject::getSeason,
+                        Collectors.averagingDouble(EpisodeObject::getRating)));         // Create a Map Object (key-value)
+                                                                                        // using Collectors
+        System.out.println(ratingPerSeason);*/
+
+        // Getting new statistics
+        /*DoubleSummaryStatistics statistics = episodes.stream()              // Class to get many statistics operations
+                .filter(e -> e.getRating() > 0.0)
+                .collect(Collectors.summarizingDouble(EpisodeObject::getRating));
+
+        System.out.println("Statistics: " + statistics);                    // Show some statistics (length, sum, min, max, average, max)
+        System.out.println("rating average: " + statistics.getAverage());
+        System.out.println("Worst episode by rating: " + statistics.getMin());
+        System.out.println("Best episode by rating: " + statistics.getMax());*/
+
+        // Getting statistics per season
+        Map<Integer, DoubleSummaryStatistics> statisticsPerSeason = episodes.stream()
+                .filter(e -> e.getRating() > 0.0)
+                .collect(Collectors.groupingBy(EpisodeObject::getSeason,
+                        Collectors.summarizingDouble(EpisodeObject::getRating)));
+
+        statisticsPerSeason.forEach((s, stat) -> System.out.println("Season: " + s
+                + " statistics: " + stat ));
     }
 }
